7ef8ec8c91d434bb5bfe7623a01404929513c1a7
will <will@twilio.com>
2012-12-11 03:05:49 -0800

    added WiFly to Serial example

---
+++ examples/WiFlyNotifyrSerial/Credentials.h
@@ -0,0 +1,11 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// Wifi parameters
+char SSID[] = "hackernet";
+char PASSPHRASE[] = "supersecret";
+
+// Notifyr KEY
+String NOTIFYR_KEY = "MY-NOTIFYR-KEY";
+
+#endif
